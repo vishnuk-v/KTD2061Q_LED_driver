38903b208d6922fe4f733cc0461d21c36eae2a9d
vishnuk-v <vishnuk@srushty.com>
2024-10-08 17:25:34 +0530

    serail input based LED code

--- src/main.cpp
+++ src/main.cpp
@@ -1,152 +1,330 @@
 #include <Arduino.h>
+
+
 #include <Wire.h>
 
-#define KTD2061Q    0x68                     // slave address
-#define CTRL_REG    0x02                     // control register 
-
-#define IRED0_REG   0x03                    // color0 register and to set the current in mA with 125uA steps
-#define IGRN0_REG   0x04
-#define IBLU0_REG   0x05
-#define IRED1_REG   0x06                    // color1 register
-#define IGRN1_REG   0x07
-#define IBLU1_REG   0x08
-
-#define ISELA12_REG 0x09                   // selection register
-#define ISELA34_REG 0x0A
-#define ISELB12_REG 0x0B
-#define ISELB34_REG 0x0C
-#define ISELC12_REG 0x0D
-#define ISELC34_REG 0x0E
-
-
-// A bus three RGB LEDS 
-// B bus two RGB LEDS
-// C bus four RGB LEDS
-// total 9 LEDS for POWER/RPM , BLE and TIMER indications
-// A1, A2, A3 PINS 6, 7, 9
-// B1, B2 PINS 11, 12
-// C1, C2, C3, C4 PINS 16, 17, 18, 19
-// by default the LED will be in shutdown mode
-// to operate in normal mode 
-
-
-// color settings registers 0x03 to 0x08
-
-// selection register 0x09 to 0x0E 
-
-// enable the normal mode
-
-void enable_normal_mode(){
-  Wire.beginTransmission(KTD2061Q);
-  
-  Wire.write(CTRL_REG);                   // normal mode enable
-  Wire.write(0x80);                       //10000000 will enable normal mode and supply a current of 0 to 24mA
+#define I2C_SDA 8
+#define I2C_SCL 9
+static int count_green=0;
+static int count_blue=0;
+static int count_red=0;
+static int count_yellow=0;
+void red_led();
+void writeRegister(uint8_t slaveAddress, uint8_t registerAddress, uint8_t payload) {
+  Wire.beginTransmission(slaveAddress);
+  // Writing values to various registers
+  Wire.write(registerAddress);
+  Wire.write(payload);  // Value for the register
   Wire.endTransmission();
 }
-
-void set_green_color(){
-  Wire.beginTransmission(KTD2061Q);
-  Wire.write(IBLU0_REG);
-  Wire.write(0);
-  Wire.write(IBLU1_REG);
-  Wire.write(0);
-  Wire.write(IRED0_REG);
-  Wire.write(0);
-  Wire.write(IRED1_REG);
-  Wire.write(0);
-  Wire.write(ISELA12_REG);                // selecting A bus Two LEDS
-  Wire.write(0xAA);                       // green color for two LEDS,- 1010 1010 
-
-  Wire.write(ISELA34_REG);                // selecting third LED - green color
-  Wire.write(0xA0);                       // 1010 0000 
-  Wire.endTransmission();                 // blinking LED part to be added 
-
-
-}
-
-void set_blue_color(){
-  Wire.beginTransmission(KTD2061Q);
-  Wire.write(IGRN0_REG);
-  Wire.write(0);
-  Wire.write(IGRN1_REG);
-  Wire.write(0);
-  Wire.write(IRED0_REG);
-  Wire.write(0);
-  Wire.write(IRED1_REG);
-  Wire.write(0);
-  Wire.write(ISELB12_REG);
-
-  Wire.write(0x90);                       //  blue color for two LEDS 1001 1100 
+void readRegister(uint8_t slaveAddress, uint8_t registerAddress) {
+  Wire.beginTransmission(slaveAddress);
+  Wire.write(registerAddress);  // Point to the register we want to read
   Wire.endTransmission();
-
-
+  Wire.requestFrom(slaveAddress, 1);  // Request 1 byte from the device
+  if (Wire.available()) {
+    uint8_t readValue = Wire.read();  // Read the byte
+    // Here you can process readValue as needed
+    // For example, you might print it or store it for verification
+//     Serial.print("Register 0x");
+//     Serial.print(registerAddress, HEX);
+//     Serial.print(": 0x");
+//     Serial.println(readValue, HEX);
+  }
 }
-
-void set_yellow_color(){
-
-  Wire.beginTransmission(KTD2061Q);
-  Wire.write(IRED0_REG);
-  Wire.write(0);
-  Wire.write(IRED1_REG);
-  Wire.write(0);
-  Wire.write(ISELB12_REG);
-  Wire.write(0x0D);                       // selecting bus B for Yellow
-
-  Wire.write(ISELC12_REG);                // Selecting C bus for two LEDS
-  Wire.write(0xDD);                       // Yellow color for two LEDS
-
-  Wire.write(ISELC34_REG);
-  Wire.write(0xDD);                       // yellow color for remianing two LEDS
+void readRegister_16(uint8_t slaveAddress, uint8_t registerAddress) {
+  Wire.beginTransmission(slaveAddress);
+  Wire.write(registerAddress);  // Point to the register we want to read
   Wire.endTransmission();
+  Wire.requestFrom(slaveAddress, 2);  // Request 1 byte from the device
+  if (Wire.available()) {
+    uint16_t readValue = Wire.read();  // Read the byte
+    // Here you can process readValue as needed
+    // For example, you might print it or store it for verification
+//     Serial.print("Register 0x");
+//     Serial.print(registerAddress, HEX);
+//     Serial.print(": 0x");
+//     Serial.println(readValue, HEX);
+  }
 }
-
-void set_red_color(){
-  Wire.beginTransmission(KTD2061Q);
-  Wire.write(IBLU0_REG);
-  Wire.write(0);
-  Wire.write(IBLU1_REG);
-  Wire.write(0);
-  Wire.write(IGRN1_REG);
-  Wire.write(0);
-  Wire.write(IGRN0_REG);
-  Wire.write(0);
-
-  Wire.write(ISELA12_REG);                // Selecting bus A 
-  Wire.write(0xCC);                       // red color in case of error or warning 1100 1100
-
-  Wire.write(ISELA34_REG);                // selecting bus B
-  Wire.write(0xC0);                       // red color for thrid LED
-
-  Wire.write(ISELB12_REG);                // selecting bus B 
-  Wire.write(0xCC);                       // red color for BLE LED and timer LED 1
-
-  Wire.write(ISELC12_REG);                // selecting bus C
-  Wire.write(0xCC);                       // red color for timer LEDS
-
-  Wire.write(ISELC34_REG);                // selecting bus C
-  Wire.write(0XCC);                       // red color for timer LEDS
-  Wire.endTransmission();
-}
-
 void setup() {
-Wire.setPins(21,22);
-Wire.begin();
-Serial.begin(115200);
-Serial.println("Setup finished");
-enable_normal_mode();
-Serial.println("Normal mode");
-set_blue_color();
-delay(100);
-set_green_color();
-delay(100);
-set_yellow_color();
-delay(100);
-set_red_color();
-delay(100);
+  Serial.begin(115200);
+  pinMode(I2C_SDA, INPUT_PULLUP);
+  pinMode(I2C_SCL, INPUT_PULLUP);
+  Wire.setPins(I2C_SDA, I2C_SCL);
+  Wire.begin();
 
 }
+void green_led()
+{
+    count_green++;
+    if(count_green==1)
+    {
+      writeRegister(0x68, 9, 0xD0);
+      writeRegister(0x68, 10, 0x00);
+    }
+    else if(count_green==2)
+    {
+      writeRegister(0x68, 9, 0xD0);
+      writeRegister(0x68, 10, 0xD0);
+    }
+    else if(count_green==3)
+    {
+      writeRegister(0x68, 9, 0xDD);
+      writeRegister(0x68, 10, 0xD0);
+    }
+    else if(count_green==4)
+   {
+      writeRegister(0x68, 9, 0x00);
+      writeRegister(0x68, 10, 0x00);
+   }
+    else if(count_green>4)
+    {
+      count_green=0;
+      green_led();
+    }
+}
+void blue_led()
+{
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  count_blue++;
+  if(count_blue==1 && count_yellow==0)
+  {
+    writeRegister(0x68, 11, 0xE0); 
+    writeRegister(0x68, 12, 0x00);    
+  }
+  else if(count_blue==1 && (count_yellow==1 || count_yellow==2 || count_yellow==3 || count_yellow==4 || count_yellow==5 ))
+  {
+    writeRegister(0x68, 11, 0xE9);
+    writeRegister(0x68, 12, 0x00); 
+  }
+  else if(count_blue>1 && (count_yellow==1 || count_yellow==2 || count_yellow==3 || count_yellow==4 || count_yellow==5 ))
+  {
+    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 12, 0x00);       
+    count_blue=0;
+    
+//    blue_led();
+  }
+  else if(count_blue==0 &&(count_yellow==1 || count_yellow==2 || count_yellow==3 || count_yellow==4 || count_yellow==5 ))
+   {
+    writeRegister(0x68, 11, 0x09);
+    writeRegister(0x68, 12, 0x00); 
+  }
+  else if(count_blue==0 && count_yellow==0)
+  {
+    writeRegister(0x68, 11, 0x00);
+    writeRegister(0x68, 12, 0x00); 
+  }
+  else if(count_blue>1 && count_yellow==0)
+  {
+    writeRegister(0x68, 11, 0x00);
+    writeRegister(0x68, 12, 0x00); 
+    count_blue=0;
+  }
+}
+
+void yellow_led()
+{
+
+  count_yellow++;
+  if(count_yellow==1 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09); 
+//    writeRegister(0x68, 11, 0x09);    
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==1 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE9);    
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==2 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE9);
+//    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 13, 0x09);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==3 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE9);
+//    writeRegister(0x68, 11, 0x09);
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==4 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE9);
+//    writeRegister(0x68, 11, 0x09);
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x90);
+  }
+  else if(count_yellow==5 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE9);
+//    writeRegister(0x68, 11, 0x09);
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x99);
+  }
+  else if(count_yellow>5 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE0); 
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+    count_yellow=0;
+//    yellow_led();     
+  }
+ else if(count_yellow==1 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09);    
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==2 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 13, 0x09);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==3 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x00);
+  }
+  else if(count_yellow==4 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x90);
+  }
+  else if(count_yellow==5 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x09); 
+    writeRegister(0x68, 13, 0x99);
+    writeRegister(0x68, 14, 0x99);
+  }
+  else if(count_yellow>5 && count_blue==0)
+  {
+    writeRegister(0x68, 11, 0x00); 
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+    count_yellow=0;
+//    yellow_led(); 
+  }
+    else if(count_yellow==0 && count_blue==1)
+  {
+    writeRegister(0x68, 11, 0xE0); 
+    writeRegister(0x68, 13, 0x00);
+    writeRegister(0x68, 14, 0x00);
+  }
+}
+void led_off()
+{
+  writeRegister(0x68, 9, 0x00);
+  writeRegister(0x68, 10, 0x00);
+  writeRegister(0x68, 11, 0x00);
+  writeRegister(0x68, 12, 0x00);
+  writeRegister(0x68, 13, 0x00);
+  writeRegister(0x68, 14, 0x00);
+  count_red=0;
+  count_green=0;
+  count_blue=0;
+  count_yellow=0;
+}
+void red_led()
+{
+
+  count_red++;
+  if(count_red==1)
+  {
+    writeRegister(0x68, 9, 0xBB);
+    writeRegister(0x68, 10, 0xBB);
+    writeRegister(0x68, 11, 0xBB);
+    writeRegister(0x68, 12, 0xBB);
+    writeRegister(0x68, 13, 0xBB);
+    writeRegister(0x68, 14, 0xBB);
+//    red_led();
+//    delay()
+  }
+
+  else if(count_red>1)
+  {
+    led_off();
+//    red_led();
+  }
+}
+void loop() 
+{
+char user_input=Serial.read();
+// LED Driver Configuration
+  writeRegister(0x68, 3, 0x28);
+  writeRegister(0x68, 4, 0x28);
+  writeRegister(0x68, 5, 0x28);
+  writeRegister(0x68, 6, 0x00);
+  writeRegister(0x68, 7, 0x00);
+  writeRegister(0x68, 8, 0x00);
+  writeRegister(0x68, 2, 0x80);
+  readRegister(0x68, 0);
+  if(user_input == 'g')
+  {
+    if(count_red==0)
+    {
+      green_led();
+    }
+    else
+    {
+      writeRegister(0x68, 9, 0xBB);
+      writeRegister(0x68, 10, 0xBB);
+      writeRegister(0x68, 11, 0xBB);
+      writeRegister(0x68, 12, 0xBB);
+      writeRegister(0x68, 13, 0xBB);
+      writeRegister(0x68, 14, 0xBB);
+//      red_led();
+    }
+  }
+  else if(user_input == 'b')
+  {
+    if(count_red==0)
+    {
+      blue_led();
+    }
+    else
+    {
+//      red_led();
+    writeRegister(0x68, 9, 0xBB);
+    writeRegister(0x68, 10, 0xBB);
+    writeRegister(0x68, 11, 0xBB);
+    writeRegister(0x68, 12, 0xBB);
+    writeRegister(0x68, 13, 0xBB);
+    writeRegister(0x68, 14, 0xBB);
+    }
+  }
+
+//   delay(5000);
+ else if(user_input=='y')
+ {
+    if(count_red==0)
+    {
+      yellow_led();
+    }
+    else
+    {
+//      red_led();
+    writeRegister(0x68, 9, 0xBB);
+    writeRegister(0x68, 10, 0xBB);
+    writeRegister(0x68, 11, 0xBB);
+    writeRegister(0x68, 12, 0xBB);
+    writeRegister(0x68, 13, 0xBB);
+    writeRegister(0x68, 14, 0xBB);
+    }
+ }
+ else if(user_input=='r')
+ {
+    red_led();
+ }
 }
 
